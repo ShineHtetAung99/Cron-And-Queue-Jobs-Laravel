7eb8ca2f8fde3601e2cb159f4bfaefa92292648c
Shine Htet Aung <96423422+ShineHtetAung99@users.noreply.github.com>
2024-10-21 11:09:42 +0630

    queue-job

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+$users = ['mgmg@gmail.com','aungaung@gmail.com','tuntun@gmail.com','hlahla@gmail.com'];
+Route::get('/send-mail', function () use($users) {
+    foreach ($users as $user) {
+        Mail::to($user)->queue(new SubscriberMail('hello'));
+    }
+    return redirect('/');
+});
