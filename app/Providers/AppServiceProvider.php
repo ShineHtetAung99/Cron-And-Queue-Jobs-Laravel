fa0ce33bca2be377d737aa1628dfc49866cee597
Shine Htet Aung <96423422+ShineHtetAung99@users.noreply.github.com>
2024-10-30 15:55:35 +0630

    add-ngrok-setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +19,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(Request $request): void
     {
-        //
+        // ngrok set up
+        if (str_ends_with($request->getHost(), '.ngrok-free.app')) {
+            URL::forceScheme('https');
+        }
     }
 }
